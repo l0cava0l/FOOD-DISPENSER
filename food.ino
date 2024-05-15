a5862e3880466c5a3f1daa913ac4eea6b9027ed8
l0cava0l <145013376+l0cava0l@users.noreply.github.com>
2024-05-15 17:48:34 +0200

    Add files via upload

---
+++ food.ino
@@ -0,0 +1,223 @@
+//FOOD DISPENSER
+#include <Servo.h>
+
+#define trigger 11
+#define echo 10
+#define pompa A0
+const int servoPin = 12;
+
+//display 7 segmenti
+unsigned const int A = 2;
+unsigned const int B = 3;
+unsigned const int C = 4;
+unsigned const int D = 5;
+unsigned const int E = 6;
+unsigned const int F = 7;
+unsigned const int G = 8;
+unsigned const int H = 9;
+
+int num_erog = 0;
+
+Servo myservo;
+
+float time=0,distance=0;
+ 
+void setup()
+{
+ //Serial.begin(9600);
+ //display
+  pinMode(A, OUTPUT);
+  pinMode(B, OUTPUT);
+  pinMode(C, OUTPUT);
+  pinMode(D, OUTPUT);
+  pinMode(E, OUTPUT);
+  pinMode(F, OUTPUT);
+  pinMode(G, OUTPUT);
+  pinMode(H, OUTPUT);
+
+ //sensore
+ pinMode(trigger,OUTPUT);
+ pinMode(echo,INPUT);
+
+ //crocchette
+ myservo.attach(servoPin);
+ myservo.write(100);
+
+ //acqua
+ pinMode(pompa,OUTPUT);
+
+ delay(1000);
+}
+ 
+void zero(void) {
+  digitalWrite(A, HIGH);
+  digitalWrite(B, HIGH);
+  digitalWrite(C, HIGH);
+  digitalWrite(D, HIGH);
+  digitalWrite(E, HIGH);
+  digitalWrite(F, HIGH);
+  digitalWrite(G, LOW);
+  digitalWrite(H, LOW);
+}
+
+void one(void) {
+  digitalWrite(A, LOW);
+  digitalWrite(B, HIGH);
+  digitalWrite(C, HIGH);
+  digitalWrite(D, LOW);
+  digitalWrite(E, LOW);
+  digitalWrite(F, LOW);
+  digitalWrite(G, LOW);
+  digitalWrite(H, LOW);
+}
+
+void two(void) {
+  digitalWrite(A, HIGH);
+  digitalWrite(B, HIGH);
+  digitalWrite(C, LOW);
+  digitalWrite(D, HIGH);
+  digitalWrite(E, HIGH);
+  digitalWrite(F, LOW);
+  digitalWrite(G, HIGH);
+  digitalWrite(H, LOW);
+}
+
+void three(void) {
+  digitalWrite(A, HIGH);
+  digitalWrite(B, HIGH);
+  digitalWrite(C, HIGH);
+  digitalWrite(D, HIGH);
+  digitalWrite(E, LOW);
+  digitalWrite(F, LOW);
+  digitalWrite(G, HIGH);
+  digitalWrite(H, LOW);
+}
+
+void four(void) {
+  digitalWrite(A, LOW);
+  digitalWrite(B, HIGH);
+  digitalWrite(C, HIGH);
+  digitalWrite(D, LOW);
+  digitalWrite(E, LOW);
+  digitalWrite(F, HIGH);
+  digitalWrite(G, HIGH);
+  digitalWrite(H, LOW);
+}
+
+void five(void) {
+  digitalWrite(A, HIGH);
+  digitalWrite(B, LOW);
+  digitalWrite(C, HIGH);
+  digitalWrite(D, HIGH);
+  digitalWrite(E, LOW);
+  digitalWrite(F, HIGH);
+  digitalWrite(G, HIGH);
+  digitalWrite(H, LOW);
+}
+
+void six(void) {
+  digitalWrite(A, HIGH);
+  digitalWrite(B, LOW);
+  digitalWrite(C, HIGH);
+  digitalWrite(D, HIGH);
+  digitalWrite(E, HIGH);
+  digitalWrite(F, HIGH);
+  digitalWrite(G, HIGH);
+  digitalWrite(H, LOW);
+}
+
+void seven(void) {
+  digitalWrite(A, HIGH);
+  digitalWrite(B, HIGH);
+  digitalWrite(C, HIGH);
+  digitalWrite(D, LOW);
+  digitalWrite(E, LOW);
+  digitalWrite(F, LOW);
+  digitalWrite(G, LOW);
+  digitalWrite(H, LOW);
+}
+
+void eight(void) {
+  digitalWrite(A, HIGH);
+  digitalWrite(B, HIGH);
+  digitalWrite(C, HIGH);
+  digitalWrite(D, HIGH);
+  digitalWrite(E, HIGH);
+  digitalWrite(F, HIGH);
+  digitalWrite(G, HIGH);
+  digitalWrite(H, LOW);
+}
+
+void nine(void) {
+  digitalWrite(A, HIGH);
+  digitalWrite(B, HIGH);
+  digitalWrite(C, HIGH);
+  digitalWrite(D, HIGH);
+  digitalWrite(E, LOW);
+  digitalWrite(F, HIGH);
+  digitalWrite(G, HIGH);
+  digitalWrite(H, LOW);
+}
+
+void measure_distance()
+{
+ digitalWrite(trigger,LOW);
+ delayMicroseconds(2);
+ digitalWrite(trigger,HIGH);
+ delayMicroseconds(10);
+ digitalWrite(trigger,LOW);
+ delayMicroseconds(2);
+ time=pulseIn(echo,HIGH);
+ distance=time*200/20000;
+}
+
+void loop()
+{
+ measure_distance();
+
+//massimo 3 erogazioni al giorno
+ if(distance<10) //&& num_erog<3
+ {
+  num_erog++;
+  delay(500);
+  myservo.write(40);
+  delay(1000);
+  myservo.write(100);
+  digitalWrite(pompa,HIGH);
+  delay(5000);
+  digitalWrite(pompa,LOW);
+  for (int i = 0; i < 6; i++) {
+    nine();
+    delay(1000);
+    Serial.println("prova");
+    
+    eight();
+    delay(1000);
+    
+    seven();
+    delay(1000);
+    
+    six();
+    delay(1000);
+    
+    five();
+    delay(1000);
+    
+    four();
+    delay(1000);
+    
+    three();
+    delay(1000);
+    
+    two();
+    delay(1000);
+    
+    one();
+    delay(1000);
+    
+    zero();
+    delay(1000);
+  }
+  delay(1000);
+  }
+}
